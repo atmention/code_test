9301ef65a54810a91da6e5db1451ad5cbef536c2
atmention <tpc303@gmail.com>
2021-11-07 21:06:05 -0700

    built pages and css

---
+++ assets/js/quiz.js
@@ -0,0 +1,30 @@
+const questions = [{
+    question: 'Light recon vehicles with rear mounted chainguns are called:',
+    options: ['banshees', 'Tanks', 'Mongooses', 'Warthog'],
+    answer: 3,
+}, {
+
+    question: 'Light recon vehicles with rear mounted chainguns are called:',
+    options: ['banshees', 'Tanks', 'Mongooses', 'Warthog'],
+    answer: 3,
+},
+{
+    question: 'Light recon vehicles with rear mounted chainguns are called:',
+    options: ['banshees', 'Tanks', 'Mongooses', 'Warthog'],
+    answer: 3,
+},
+{
+    question: 'Light recon vehicles with rear mounted chainguns are called:',
+    options: ['banshees', 'Tanks', 'Mongooses', 'Warthog'],
+    answer: 3,
+},
+{
+    question: 'Light recon vehicles with rear mounted chainguns are called:',
+    options: ['banshees', 'Tanks', 'Mongooses', 'Warthog'],
+    answer: 3,
+},
+]
+// Select and store the start button
+document.getElementById('start-button').addEventListener("click", function()) {
+window.location.href ='quiz.html',    
+}
