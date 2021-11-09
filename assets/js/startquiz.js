52b75fcd9b3c06e261e92870c34b23007ccf6a2b
atmention <tpc303@gmail.com>
2021-11-09 10:40:13 -0700

    creation of buttons and logic for answers

--- assets/js/startquiz.js
+++ assets/js/startquiz.js
@@ -1,34 +1,6 @@
-const questions = [{
-    question: 'How many points for a tech fall in Greco:',
-    options: ['15', '10', '8', '25'],
-    answer: 3,
-}, {
-
-    question: 'Highest percentage takedown finishes in the NCAA:',
-    options: ['Double leg', 'Single leg', 'Suplex', 'Headlock'],
-    answer: 2,
-},
-{
-    question: 'What is PA wrestling known for:',
-    options: ['Mat wrestling', 'Takedowns', 'Freestyle', 'Greco'],
-    answer: 1,
-},
-{
-    question: 'Best current Team USA wrestler:',
-    options: ['Jordan Burroughs', 'Adaline Gray', 'Kyle Dake', 'Spencer Lee'],
-    answer: 1,
-},
-{
-    question: '2021 Hodge Trophy winner:',
-    options: ['Gable Steveson', 'Mosha Schwartz', 'Mr. Fasttwitch', 'Spencer Lee'],
-    answer: 4,
-},
-]
-// Select and store the start button
-document.getElementById('start-button').addEventListener("click", function()) {
-window.location.href ='quiz.html',    
-}
-
-startQuiz() {
-    window.location.href ='quiz.html',
-}
+// Select & store the start button
+// Add event listener for clicking on the 
+// start button to begin the quiz.
+document.getElementById("start-button").addEventListener("click", function() {
+    window.location.href = 'quiz.html';
+})
